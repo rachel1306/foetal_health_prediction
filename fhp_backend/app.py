1271be59463b7bc7847f191285b0191bc57fafb2
Goutham-R <gouthamr000@gmail.com>
2022-08-02 15:28:08 +0530

    Version 1

--- fhp_backend/app.py
+++ fhp_backend/app.py
@@ -4,7 +4,7 @@ import keras
 
 
 app = Flask(__name__)
-model = keras.models.load_model('./model.h5')
+model = keras.models.load_model('./model_new.h5')
 
 @app.route('/')
 def home():
@@ -15,43 +15,39 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    #if prediction == 0:
-     #   prediction_text='The health of the foetus appears normal!'
-    #elif prediction == 1:
-     #   prediction_text='There appears to be slight discrepencies regarding the health of the foetus. Contact your health provider for further clarifications.'
-    #else:  
-     #   prediction_text='The model predicted an abnormal value. Please contact your health provider immediately.'
-    #return render_template('index.html', prediction_text)
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features).reshape(1,-1)]
+    pred = model.predict(final_features)
+    pred = pred[0,:]
+    pred = pred.astype(int)
+    prediction = np.bincount(pred).argmax()
     if prediction == 0:
-        return {
-            'number':'0',
-            'description':'The health of the foetus appears normal!'
-        }
+        prediction_text='The health of the foetus appears normal!'
     elif prediction == 1:
-        return{
-            'number':'1',
-            'description': 'There appears to be slight discrepencies regarding the health of the foetus. Contact your health provider for further clarifications.'
-        }
-    else:  
-        return{
-            'number':'2',
-            'description':'The model predicted an abnormal value. Please contact your health provider immediately.'
-        }
+        prediction_text='There appears to be slight discrepencies regarding the health of the foetus. Contact your health provider for further clarifications.'
+    elif prediction == 2:  
+       prediction_text='The model predicted an abnormal value. Please contact your health provider immediately.'
+    else :
+        prediction_text='Lemon'
+    return render_template('index.html',prediction_text = prediction_text)
+    #if prediction == 0:
+    #    return {
+    #        'number':'0',
+    #        'description':'The health of the foetus appears normal!'
+    #    }
+    #elif prediction == 1:
+     #   return{
+     #       'number':'1',
+    #        'description': 'There appears to be slight discrepencies regarding the health of the foetus. Contact your health provider for further clarifications.'
+    #    }
+   # else:  
+     #   return{
+    #        'number':'2',
+     #       'description':'The model predicted an abnormal value. Please contact your health provider immediately.'
+     #   }
 
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
